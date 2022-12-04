e3adafdef882a5c9163e3a750f04fd69ad0a805b
teldufalsari <idunaev71@gmail.com>
2022-12-04 13:58:00 +0300

    Add missing proto implementation

--- src/core/proto.rs
+++ src/core/proto.rs
@@ -3,7 +3,7 @@ use rmp_serde;
 
 
 #[derive(Debug, Serialize, Deserialize)]
-enum Type {
+pub enum Type {
     Request,
     Accept,
     Deny,
@@ -14,37 +14,43 @@ enum Type {
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-struct Message {
+pub struct Message {
     t: Type,
     d: Option<Vec<u8>>,
 }
 
 impl Message {
-    fn new_request() -> Self {
+    pub fn new_request() -> Self {
         Self { t: Type::Request, d: None }
     }
 
-    fn new_accept() -> Self {
+    pub fn new_accept() -> Self {
         Self { t: Type::Accept, d: None }
     }
 
-    fn new_deny() -> Self {
+    pub fn new_deny() -> Self {
         Self { t: Type::Deny, d: None }
     }
 
-    fn new_confirm() -> Self {
+    pub fn new_confirm() -> Self {
         Self {t: Type::Confirm, d: None}
     }
 
-    fn new_speak_plain(payload: Vec<u8>) -> Self {
+    pub fn new_speak_plain(payload: Vec<u8>) -> Self {
         Self { t: Type::SpeakPlain, d: Some(payload) }
     }
 
-    fn new_close() -> Self {
+    pub fn new_close() -> Self {
         Self {t: Type::Close, d: None}
     }
 
-    fn serialize(&self) -> Vec<u8> {
+    // TODO: return error instead of expecting
+    pub fn serialize(&self) -> Vec<u8> {
         rmp_serde::to_vec(&self).expect("Serialization successfull")
+    }
+
+    // TODO: return error instead of expecting
+    pub fn deserialize(bytes: &[u8]) -> Self {
+        rmp_serde::from_slice(bytes).expect("Deserialization successfull")
     }
 }
